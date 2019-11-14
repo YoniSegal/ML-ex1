efe32c20fe3bc861152561f3fc99ddc83e72f027
Yoni Segal <yonisegal13@gmail.com>
2019-11-14 20:09:03 +0200

    k-means works

--- ex1.py
+++ ex1.py
@@ -1,7 +1,8 @@
 # Yonatan Segal 342476611
+import sys
+
 import numpy as np
-from scipy.misc import imread
-from init_centroids import init_centroids
+from scipy.io import wavfile
 
 
 # Method returns the closest centroid to a given pixel.
@@ -31,65 +32,64 @@ def assign_groups(X, centroids, centroid_sets):
 
 # Method divides 2 3D vectors.
 def div_vector(total_centroid, size):
-    return [total_centroid[0] / size, total_centroid[1] / size, total_centroid[2] / size]
+    # return [total_centroid[0] / size, total_centroid[1] / size]
+    return [round(total_centroid[0] / size), round(total_centroid[1] / size)]
 
 
 # Method returns a new array of centroids based on the average pixel values.
 def update_centroids(centroid_sets):
     new_centroids = []
-    total_centroid = [0., 0., 0.]
+    total_centroid = [0., 0.]
     for cluster in centroid_sets:
-        for colour in cluster:
-            total_centroid += colour
+        for sound in cluster:
+            total_centroid += sound
         average_centroid = div_vector(total_centroid, len(cluster))
         new_centroids.append(average_centroid)
-        total_centroid = [0., 0., 0.]
+        total_centroid = [0., 0.]
     return np.asarray(new_centroids)
 
 
 # Method prints all centroids in a given iteration.
 def print_centroids(centroids, i, k):
-    final_string = "iter " + str(i) + ": "
+    final_string = "[iter " + str(i) + "]:"
     string = "["
     counter = 0
     for c in centroids:
         first = str(np.floor((c[0] * 100)) / 100)
         second = str(np.floor((c[1] * 100)) / 100)
-        third = str(np.floor((c[2] * 100)) / 100)
         if first == "0.0":
             first = "0."
         if second == "0.0":
             second = "0."
-        if third == "0.0":
-            third = "0."
         counter += 1
-        string += first + ", " + second + ", " + third + "]"
+        string += first[:-1] + " " + second[:-1] + "]"
         if counter != k:
-            string += ", ["
+            string += ",["
     final_string += string
-    print(final_string)
+    return final_string
 
 
 # Main method takes an image and uses k-means to alter pixels around given centroids.
-def main():
-    path = 'dog_2.jpeg'
-    A = imread(path)
-    A = A.astype(float) / 255.
-    img_size = A.shape
-    X = A.reshape(img_size[0] * img_size[1], img_size[2])
+if __name__ == '__main__':
+    wav = sys.argv[1]
+    cents = sys.argv[2]
+    text_file = open("test" + cents, "w")
+    fs, y = wavfile.read(wav)
+    centroids = np.loadtxt(cents)
     k = [2, 4, 8, 16]
+    k = [int(centroids.size / 2)]
+    previous = ""
     for k in k:
-        centroids = init_centroids(X, k)
-        print("k=" + str(k) + ":")
-        print_centroids(centroids, 0, k)
-        for i in range(10):
+        for i in range(30):
             # centroid_sets 1..n -> centroids 1..n
             # for each point x, if centroids[i] is closest to x, put x into centroid_sets[i]
             centroid_sets = [[] for j in range(k)]
-            assign_groups(X, centroids, centroid_sets)
+            assign_groups(y, centroids, centroid_sets)
             centroids = update_centroids(centroid_sets)
-            # print
-            print_centroids(centroids, i + 1, k)
-
-
-main()
+            temp1 = print_centroids(centroids, i, k) + "\n"
+            text_file.write(temp1)
+            print(temp1)
+            if i > 0 and temp1.find(previous[9:]) != -1:
+                break
+            previous = temp1
+    text_file.close()
